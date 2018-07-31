0a02dbd8f006c7d087eb3a3e465aaf643e2dae34
AldenSharp <aldensharp2@gmail.com>
2018-07-31 02:13:26 -0500

    Bugfix attempt.

--- app/js/services/evolutionService.js
+++ app/js/services/evolutionService.js
@@ -734,12 +734,12 @@ angular.module('app').service('evolutionService', [
     // This transformation cannot take conditions.
     function syllablePositionDeletion (word, language, transformation) {
       word.syllables.forEach((syllable) => syllable.phonemes.splice(
-        transformation.position + language.phonology.vowelCore, 1
+        transformation.position + language.phonology.vowelCore, 0
       ))
     }
 
     function languageSyllablePositionDeletion (phonotactics, vowelCore, transformation) {
-      phonotactics.splice(transformation.position + vowelCore, 1)
+      phonotactics.splice(transformation.position + vowelCore, 0)
     }
 
     function checkSyllablePositionDeletion (language, transformation, transformationLocation) {
