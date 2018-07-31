237ef7b476e631db967119858d6cf3489899f94d
AldenSharp <aldensharp2@gmail.com>
2018-07-31 01:51:04 -0500

    Bugfix attempt.

--- app/js/services/evolutionService.js
+++ app/js/services/evolutionService.js
@@ -706,7 +706,7 @@ angular.module('app').service('evolutionService', [
       let absolutePosition = transformation.position + language.phonology.vowelCore
         + (transformation.position < 0 ? 1 : 0)
       phonotactics.splice(absolutePosition, 0, [''])
-      if (transformation.postion < 0) { vowelCore++ }
+      if (transformation.postion < 0) { vowelCore = vowelCore + 1 }
     }
 
     function checkSyllablePositionInsertion (language, transformation, transformationLocation) {
@@ -740,7 +740,7 @@ angular.module('app').service('evolutionService', [
 
     function languageSyllablePositionDeletion (phonotactics, vowelCore, transformation) {
       phonotactics.splice(transformation.position + vowelCore, 1)
-      if (transformation.position < 0) { vowelCore-- }
+      if (transformation.position < 0) { vowelCore = vowelCore - 1 }
     }
 
     function checkSyllablePositionDeletion (language, transformation, transformationLocation) {
