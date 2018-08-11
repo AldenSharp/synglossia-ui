f62b1a2387d29167467d1f5805a17c95e97aa856
AldenSharp <aldensharp2@gmail.com>
2018-08-10 23:06:10 -0500

    Adding loading placeholders.

--- app/js/domain/home/homeController.js
+++ app/js/domain/home/homeController.js
@@ -1,11 +1,15 @@
 angular.module('app').controller('homeController', [ '$http', function homeController ($http) {
   let ctrl = this
 
-  let setLanguageNames = (languageNamesData) =>
+  this.retrievingData = true
+
+  function setLanguageNames (languageNamesData) {
+    ctrl.retrievingData = false
     ctrl.languageNames = languageNamesData.map((languageName) => ({
       href: '#/interface/' + languageName,
       displayName: languageName.replace('_', ' ')
     }))
+  }
 
   $http.get("https://c1hj6zyvol.execute-api.us-east-1.amazonaws.com/prod/languages")
   .then((httpResponse) => setLanguageNames(httpResponse.data))
